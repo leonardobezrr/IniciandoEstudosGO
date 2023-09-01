4516910e5022c98958b30df0deb5b29f47737e5e
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-08-31 22:18:57 -0300

    finalizando aula de variáveis

--- 2 - Variaveis/variaveis.go
+++ 2 - Variaveis/variaveis.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main(){
 	var frases string = "String 1"
-	frases2 := "String 2"
-	fmt.Println(frases)
-	fmt.Println(frases2)
+	frases2 := "String 2" //Utilizando a técnica de inferência de tipos, ele seleciona o tipo a partir do valor
+	fmt.Println(frases,frases2)
+
+	frases,frases2 = frases2,frases //Inverter de forma rápida o valor das variáveis
+
+	fmt.Println(frases,frases2)
 }
