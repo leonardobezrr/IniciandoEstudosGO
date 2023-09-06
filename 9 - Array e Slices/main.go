c145f8b4665f4a8c202250a2ca267b0f5be909f7
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-09-06 14:28:11 -0300

    finalizando primeira parte de slices

--- 9 - Array e Slices/main.go
+++ 9 - Array e Slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main (){
 	fmt.Println("Array e Slices")
@@ -15,8 +18,20 @@ func main (){
 
 	array3 := [5]string {"ola 1,","2","3","4","5"}// e assim, por inferência de tipo 
 	array3[1] = "1"
-	fmt.Println(array3)
+	fmt.Println(array3,"Array 3")
 
 	array4 := [...]int{1,2,3,4,5,6} //Vai decidir o tamanho dependendo de quantos itens tiver no array 
-	fmt.Println(array4)
+	fmt.Println(array4,"Array 4")
+
+	lista1 := []int {1,2,3,4,12,23,123,}	// SLICE Possui tamnho dinâmico, possuindo limitação apenas do tipo
+	fmt.Println(lista1,"Lista 1")
+
+	lista1 = append(lista1, 21) //Adicionando valores ao SLICE
+	fmt.Println(lista1,"Lista 1 utilizando append")
+
+	lista2 := array4[0:3]
+	fmt.Println(lista2,"Selecionando valores do Array 4")
+
+	fmt.Println(reflect.TypeOf(lista1))
+	fmt.Println(reflect.TypeOf(array4)) // Possuem tipos diferentes 
 }
