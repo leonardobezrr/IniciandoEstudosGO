c03935d0e981c01a92d0c90ebeaa83fdff0a36a0
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-09-06 09:40:34 -0300

    iniciando noção de ponteiros em go

---
+++ 8 - Ponteiros/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("\nPonteiros")
+
+	var variavel1 int = 10
+	var variavel2 int = variavel1
+	fmt.Println(variavel1,variavel2)
+
+	variavel1 ++
+	fmt.Println(variavel1,variavel2)
+} 
